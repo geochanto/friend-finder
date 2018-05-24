4e2cb6ca96210825d4d49471ceffa39b09285f1b
George Chanturidze <georgechanturidze@georges-mbp.attlocal.net>
2018-05-24 10:04:41 -0700

    got basics working

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+  {
+    name: "Georgio",
+    photo: "https://www.healthypawspetinsurance.com/Images/V3/DogAndPuppyInsurance/Dog_CTA_Desktop_HeroImage.jpg",
+    scores: [
+      5,
+      4,
+      4,
+      3,
+      2,
+      1,
+      3,
+      5,
+      4,
+      2
+    ]
+  }
+];
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
